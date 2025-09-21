f46e7dd3575e8f3af69da35e05bd9d230469f77e
vickllny <vickllny@163.com>
2025-09-21 23:36:06 +0800

    归并排序 TODO 小和问题

--- src/main/java/com/vickllny/MergeSort.java
+++ src/main/java/com/vickllny/MergeSort.java
@@ -25,16 +25,44 @@ public class MergeSort {
         int mid = left + ((right - left) >> 1);
         process(arr, left, mid);
         process(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        merge(arr, left, mid, right, 0);
     }
 
-    static void merge(int[] arr, int left, int mid, int right){
+
+    public static int smallerNumber(int[] arr){
+        if(arr == null || arr.length  == 1){
+            return -1;
+        }
+        return smallerNumberProcess(arr, 0, arr.length - 1);
+    }
+
+    static int smallerNumberProcess(int[] arr, int left, int right){
+        if(left == right){
+            return 0;
+        }
+        int mid = left + ((right - left) >> 1);
+        process(arr, left, mid);
+        process(arr, mid + 1, right);
+        int value = 0;
+        merge(arr, left, mid, right, value);
+        return value;
+    }
+
+    static void merge(int[] arr, int left, int mid, int right, int value){
         int[] help = new int[right - left + 1];
         int i = 0;
         int p1 = left;
         int p2 = mid + 1;
         while (p1 <= mid && p2 <= right){
-            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+            if(arr[p1] <= arr[p2]){
+                help[i++] = arr[p1++];
+                if(arr[p1] != arr[p2]){
+                    value += help[i - 1];
+                }
+            }else {
+                help[i++] = arr[p2++];
+            }
+
         }
         while (p1 <= mid){
             help[i++] = arr[p1++];
